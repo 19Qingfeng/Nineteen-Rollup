23f4f1ca7ec98ba3ef97380c80acf43ba530ae79
wang.haoyu <wanghaoyu@wanghaoyudeMacBook-Pro.local>
2020-09-24 22:23:22 +0800

    flybox

--- src/components/flyBox/index.vue
+++ src/components/flyBox/index.vue
@@ -1,16 +1,24 @@
 <template>
-    <div style="width:600px;height:600px;background-color:#333">
+    <div
+        class="m_fly_box"
+        ref="wapper"
+    >
+        <!-- 自适应布局把握以下要点
+            onMounted时获取最外层div的宽和高(最外层div宽高继承层100%)
+            然后svg的width和height等于最外层宽高 此时viewBox默认是宽高是viewPort
+            然后内层矩形和外层矩形的对应大小要根据宽高变化 这里使用computed获得path的d
+            之后动画animateMotion的path也要随着矩形变化而变化（动画环绕矩形点运动）
+     -->
         <svg
-            style="margin-top:20px"
-            height="400"
-            width="400"
+            :width="width"
+            :height="height"
         >
             <defs>
                 <mask id="mask">
                     <circle
                         cx="0"
                         cy="0"
-                        r="100"
+                        :r="starArea"
                         fill="url(#gradient)"
                     >
                         <!-- mask -->
@@ -18,12 +26,19 @@
                         <!-- fill:black 纯黑色全透明 顶层隐藏 显示底层 -->
                         <!-- 结合opacity或者fill的颜色透明度 可以达到蒙蔽颜色结合的效果 -->
                         <!-- 这里就是使用了渐变白 达到了 不透明->全透明效果的视觉渐变 -->
+                        <!-- mask坐标系统按照svg画布 -->
 
                         <!-- 最后再让蒙版按照外层矩形运行起来 就实现了流行环绕视觉效果 -->
+
+                        <!-- 注意这里的rotate 这个属性实现了运动过程中动画元素的旋转 -->
+                        <!-- 可以指定固定值，固定角度并不会跟随动画而变动
+                            也可以在路径交汇点自动变化auto表示跟随动画自动旋转(右转)auto-reverse左
+                            https://developer.mozilla.org/zh-CN/docs/Web/SVG/Attribute/rotate
+                         -->
                         <animateMotion
-                            path="M 0 0 L400 0 L400 400 L0 400 Z"
-                            dur="2s"
-                            rotate="0"
+                            :path="path"
+                            :dur="`${+ duration}s`"
+                            rotate="auto"
                             repeatCount="indefinite"
                         ></animateMotion>
                     </circle>
@@ -32,7 +47,7 @@
                     id="gradient"
                     cx=".5"
                     cy=".5"
-                    fx=".9"
+                    fx="1"
                     fy=".5"
                     r=".5"
                 >
@@ -41,7 +56,7 @@
                     <!-- fx fy 确定渐变起始点 -->
                     <!-- 既从fxfy(offset:0) -> 确定圆边界(offset:1) 完成渐变 -->
 
-                    <!-- cx cy fx fy r相对于调用者宽高的百分比 -->
+                    <!-- cx cy fx fy r相对于调用者宽高的百分比(坐标系统在调用者内部) -->
                     <stop
                         offset="0"
                         stop-color="#fff"
@@ -54,52 +69,115 @@
                     ></stop>
                 </radialGradient>
 
-                <rect
+                <path
                     id="rect"
-                    x='0'
-                    y='0'
-                    height="400"
-                    width="400"
+                    :d="path"
                     fill="none"
-                ></rect>
+                ></path>
+
+                <!-- <rect :path="path"></rect> -->
 
             </defs>
 
             <use
                 href="#rect"
-                stroke-width="3"
-                stroke="#235fa7"
+                :stroke-width="+lineWidth"
+                :stroke="lineColor"
             ></use>
             <use
                 href="#rect"
-                stroke-width="5"
-                stroke="#4fd2dd"
+                :stroke-width="+starWidth"
+                :stroke="starColor"
                 mask="url(#mask)"
             ></use>
-            <!-- <rect
-                x='0'
-                y='0'
-                height="400"
-                width="400"
-                stroke-width="3"
-                stroke="#235fa7"
-                fill="none"
-            ></rect> -->
-            <!-- <rect
-                x='0'
-                y='0'
-                height="400"
-                width="400"
-                stroke-width="5"
-                stroke="#4fd2dd"
-                fill="none"
-                mask="url(#mask)"
-            ></rect> -->
         </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+
     </div>
 </template>
 <script>
+import { onMounted, ref, computed, getCurrentInstance } from "vue";
 export default {
   name: "flyBox",
+  props: {
+    // 宽高继承外层元素
+    // 矩形边框颜色
+    lineColor: {
+      type: String,
+      default: "#235fa7",
+    },
+    // 矩形线宽
+    lineWidth: {
+      type: [Number, String],
+      default: 1,
+    },
+    // 动画线条（流行）宽度
+    starWidth: {
+      type: [Number, String],
+      default: 3,
+    },
+    // 动画线条（流星）颜色
+    starColor: {
+      type: String,
+      default: "#4fd2dd",
+    },
+    // 动画时间
+    duration: {
+      type: [Number, String],
+      default: 2,
+    },
+    // 动画线条（流行）长度
+    starArea: {
+      type: Number,
+      default: 50,
+    },
+  },
+  setup(prop, ctx) {
+    const height = ref(0);
+    const width = ref(0);
+    const wapper = ref(null);
+    const path = computed(() => {
+      return `M5 5 L${width.value - 5} 5 L${width.value - 5} ${
+        height.value - 5
+      } L5 ${height.value - 5} Z`;
+    });
+    const init = () => {
+      const dom = getCurrentInstance().ctx.$refs["wapper"];
+      height.value = dom.clientHeight;
+      width.value = dom.clientWidth;
+    };
+    onMounted(() => {
+      init();
+    });
+    return {
+      height,
+      width,
+      wapper,
+      path,
+    };
+  },
 };
 </script>
+<style lang="scss" scoped>
+.m_fly_box {
+  height: 100%;
+  width: 100%;
+  position: relative;
+  svg {
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+  }
+  .content {
+    padding: 5px;
+    height: 100%;
+    width: 100%;
+    box-sizing: border-box;
+    overflow: hidden;
+  }
+}
+</style>
